143f14334bd557a716020ed42cfd1ad46a70ee08
QJH-Ghb <79617657+QJH-Ghb@users.noreply.github.com>
2025-10-06 18:45:57 +0800

    登入介面與資料庫連結：新增 Models 與 Login 視圖

---
+++ MusicPlayer/Models/account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_DB_.Models
+{
+    public class account
+    {
+        public int ID { get; set; }
+        public string userName { get; set; }
+        public string passWord { get; set; }
+        public string email { get; set; }
+        public DateTime createTime { get; set; }
+    }
+
+}
